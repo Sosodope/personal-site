03df24978a1e9371f6ad27f3fb7617ea8edb31e2
Sosodope <sonadolkar93@hotmail.com>
2018-08-18 20:08:59 +0700

    add image and button to project panel

--- src/components/ProjectsPanel/index.jsx
+++ src/components/ProjectsPanel/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import get from 'lodash/get'
 import Link from 'gatsby-link'
+import projectsPic from '../../assets/images/latest-projects.png'
 import './style.scss'
 
 class AboutPanel extends React.Component {
@@ -10,6 +10,18 @@ class AboutPanel extends React.Component {
     const projectsBlock = (
       <div>
         <h2 className="sidebar__author-title">{projects.name}</h2>
+        <Link to="/projects">
+          <img
+            src={projectsPic}
+            className="sidebar__author-photo"
+            width="400"
+            height="400"
+            alt={projects.name}
+          />
+        </Link>
+        <Link to="/projects" className="projects__page-button">
+          Check out my projects
+        </Link>
       </div>
     )
 
